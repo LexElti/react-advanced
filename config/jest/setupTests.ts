779331edb93555e4800778082be4c72bae20e911
AlexeyKrapivin <krapivin.a@roskvartal.ru>
2023-07-12 18:40:33 +0300

    3 24 add React Testing Library

---
+++ config/jest/setupTests.ts
@@ -0,0 +1,6 @@
+// Такой файл вы могли наблюдать при create-react-app
+// jest-dom для методов типа toBeInTheDocument
+import '@testing-library/jest-dom';
+// чтоб пофиксить ошибку regeneratorRuntime is not defined
+// возникла из-за асинхрон функции
+import 'regenerator-runtime/runtime';
