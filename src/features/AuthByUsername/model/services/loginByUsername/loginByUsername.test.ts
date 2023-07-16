9db6e17f7dbddeeca7beabdc6e3b57ca98d243ea
AlexeyKrapivin <krapivin.a@roskvartal.ru>
2023-07-16 16:48:57 +0300

    5 37 Profile page. add memo

--- src/features/AuthByUsername/model/services/loginByUsername/loginByUsername.test.ts
+++ src/features/AuthByUsername/model/services/loginByUsername/loginByUsername.test.ts
@@ -7,14 +7,12 @@ import { loginByUsername } from './loginByUsername';
 
 jest.mock('axios');
 
-// чтоб замокать внутренние поля axios
-// нужно для TypeScript
 const mockedAxios = jest.mocked(axios, true);
 
 describe('loginByUsername.test', () => {
     // let dispatch: Dispatch;
     // let getState: () => StateSchema;
-
+    //
     // beforeEach(() => {
     //     dispatch = jest.fn();
     //     getState = jest.fn();
@@ -25,19 +23,19 @@ describe('loginByUsername.test', () => {
     //     mockedAxios.post.mockReturnValue(Promise.resolve({ data: userValue }));
     //     const action = loginByUsername({ username: '123', password: '123' });
     //     const result = await action(dispatch, getState, undefined);
-
+    //
     //     expect(dispatch).toHaveBeenCalledWith(userActions.setAuthData(userValue));
     //     expect(dispatch).toHaveBeenCalledTimes(3);
     //     expect(mockedAxios.post).toHaveBeenCalled();
     //     expect(result.meta.requestStatus).toBe('fulfilled');
     //     expect(result.payload).toEqual(userValue);
     // });
-
+    //
     // test('error login', async () => {
     //     mockedAxios.post.mockReturnValue(Promise.resolve({ status: 403 }));
     //     const action = loginByUsername({ username: '123', password: '123' });
     //     const result = await action(dispatch, getState, undefined);
-
+    //
     //     expect(dispatch).toHaveBeenCalledTimes(2);
     //     expect(mockedAxios.post).toHaveBeenCalled();
     //     expect(result.meta.requestStatus).toBe('rejected');
