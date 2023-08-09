d4a5a948ee21461baeeb5830e267845df047ca0a
AlexeyKrapivin <krapivin.a@roskvartal.ru>
2023-08-09 17:45:58 +0300

    13 105 BuildSlice  BuildSelector

--- scripts/clear-cache.js
+++ scripts/clear-cache.js
@@ -1,7 +1,2 @@
 // path/fs
-const path = require('path');
-const fs = require('fs');
-
-const cacheDirectory = path.resolve(__dirname, '..', 'node_modules', '.cache');
-fs.rmSync(cacheDirectory, { recursive: true, force: true });
-console.log('done');
+console.log('CLEAR CACHE');
